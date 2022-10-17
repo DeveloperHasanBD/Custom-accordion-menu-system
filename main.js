a68c9c1f4792c9d700e861e77065f19620ec8751
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-10-17 19:52:16 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-
-
-window.onload = () => {
-
-    const addClassWidget = document.querySelectorAll("#search_filter_register_widget-2 form ul li ul");
-    addClassWidget.forEach(function (e) {
-        e.parentElement.classList.add("parent-cat");
-        e.classList.add("toggle-item");
-        e.classList.add("hide-item");
+ jQuery('.r_menu_area  .menu-item-has-children').prepend('<span>Hello</span>');
+jQuery(function() {
+    jQuery(".r_menu_area  .menu-item-has-children span").click(function(event) {
+        event.preventDefault();
+        jQuery(".r_menu_area  .menu-item-has-children").find('.sub-menu').css({
+            'display': 'none'
+        });
+        jQuery(this).closest(".r_menu_area  .menu-item-has-children").find('.sub-menu').slideToggle();
     });
-
-    const elements = document.querySelectorAll('#search_filter_register_widget-2 form ul li.parent-cat');
-
-    elements.forEach(element => {
-        let btn = element.querySelector('h4');
-        var answer = element.lastElementChild;
-        var answers = document.querySelectorAll('#search_filter_register_widget-2 form ul li ul.toggle-item');
-
-        if (btn !== null && answer) {
-            btn.addEventListener('click', () => {
-                answers.forEach(ans => {
-                    if (answer !== ans) {
-                        ans.classList.add('hide-item');
-                    }
-                });
-
-                answer.classList.toggle('hide-item');
-            });
-        }
-    });
-
-}
+});
